eecb7b686ea4858313d24d186a7aa56e3e1a0174
Santiago Villar Vazquez <alu0100990522@ull.edu.es>
2019-11-28 11:24:56 +0000

    Arreglado el problema del cambio de practica

--- src/main/java/com/javadeeplearningcookbook/examples/SentenceDataPreProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.javadeeplearningcookbook.examples;
-
-import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
-
-public class SentenceDataPreProcessor {
-    public static void setPreprocessor(SentenceIterator iterator){
-        iterator.setPreProcessor(((String sentence) -> sentence.toLowerCase()));
-    }
-}
